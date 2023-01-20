4e074916f64b80dac7d0308c9e526fb2c92d71bc
johnobodai <j.obodai@alustudent.com>
2023-01-20 21:20:43 +0000

    Modified

--- RegEx.py
+++ RegEx.py
@@ -8,12 +8,14 @@ from termcolor import colored
 f = Figlet(font='standard')
 print(colored(f.renderText('RegEx Made Easy'), 'white'))
 
+Print("What Expression are you in for?")
+
 def Jokes(txt):
     Joke = (re.search("Why did the \w.+? Because \w+", txt))
     if Joke:
-        print("{} was a halarious joke, hahaha.")
+        print("{} was a halarious joke, hahaha.".format(txt))
     else:
-        print("The was no joke, you arent funny")
+        print("There was no joke, you aren't funny.")
 
 def MovieTitle(txt):
     return (re.search("\w+\s\d{4}", txt))
@@ -42,4 +44,4 @@ def IPAddress(txt):
     return ("\d{3}\.\d{3}\.{3}\.{3}")  # "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
 
 
-Jokes("Why did the guy come here? Because je wanted to")
+Jokes("asdf Why did the guy come here? Because je wanted to")
