628bc8fcaf1b113ae77b6348679453c98deef78e
johnobodai <j.obodai@alustudent.com>
2023-01-20 13:30:19 +0000

    Modified

--- RegEx.py
+++ RegEx.py
@@ -20,3 +20,6 @@ def SongLyrics(txt):
 #My codes will be allowing twittter users to creat their twitter handles using any character
 def TwitterUsername(txt):
     return (re.search("@w+", txt))
+
+def TVEpisodeTitles(txt):
+    return ("\w+ S\d\dE\d\d: \w+")
