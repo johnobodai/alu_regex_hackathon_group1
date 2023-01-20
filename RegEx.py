c6db6576fa7be6f688967e7f95a370b4ae8e0703
johnobodai <j.obodai@alustudent.com>
2023-01-20 13:29:05 +0000

    modified

--- RegEx.py
+++ RegEx.py
@@ -16,3 +16,7 @@ def MovieTitle(txt):
 
 def SongLyrics(txt):
     return (re.search("Verse \d \w+", txt))
+
+#My codes will be allowing twittter users to creat their twitter handles using any character
+def TwitterUsername(txt):
+    return (re.search("@w+", txt))
--- TwitterUsername.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import re
-
-
-#My codes will be allowing twittter users to creat their twitter handles using any character
-def TwitterUsername(txt):
-    return (re.search("@w+", txt))
