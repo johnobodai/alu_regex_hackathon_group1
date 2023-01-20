0be4094f99540a02c6db06d5d90511d8193afdc6
johnobodai <j.obodai@alustudent.com>
2023-01-20 07:46:46 +0000

    conflict resloved

--- RegEx.py
+++ RegEx.py
@@ -4,4 +4,14 @@ import re
 #My code will be allowing user to create the twitter handles with any characters
 def Twitterusername(txt):
     return (re.search("@\w+", txt))
+# Python regex that matches jokes that start with "Why did the....? and preceeded by Because..."
+import re
+from pyfiglet import Figlet
+from termcolor import colored
 
+
+f = Figlet(font='standard')
+print(colored(f.renderText('RegEx Made Easy'), 'white'))
+
+def Jokes(txt):
+    return (re.search("Why did the \w+? Because \w+", txt))
